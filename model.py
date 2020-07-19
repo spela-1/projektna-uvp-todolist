cf1b0c8ff9ecea5332a2f3521764842a7fbff0cb
spela <a.spela.bernardic@gmail.com>
2020-07-19 17:58:08 +0200

    dodan razred Projekt

--- model.py
+++ model.py
@@ -1,13 +1,30 @@
 class Naloga():
-    def __init__(self, tekst, datum=None , ura=None, prioriteta=None):
+    def __init__(self, id, tekst, datum=None , ura=None, prioriteta=None):
         self.tekst = tekst
         self.datum = datum
         self.ura = ura
         self.opravljeno = False
         self.prioriteta = prioriteta
-        
+        self.id = id
 
+
+class Projekt():
+    def __init__(self, ime):
+        self.ime = ime
+        self.seznam_nalog = []
+        self.naslednji_id = 0
     
+    def nova_naloga(self, tekst, datum=None , ura=None, prioriteta=None):
+        naloga = Naloga(self.naslednji_id, tekst, datum, ura, prioriteta)
+        self.seznam_nalog.append(naloga)
+        self.naslednji_id += 1
+
+    def izbrisi_nalogo(self, id):
+        for naloga in self.seznam_nalog:
+            if naloga.id == id:
+                self.seznam_nalog.remove(naloga)
+                break
+            
 
 
 

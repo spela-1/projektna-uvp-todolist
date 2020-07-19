95ef1ec60d7dc8dbdd1264555ec50f4f040e16ad
spela <a.spela.bernardic@gmail.com>
2020-07-19 17:28:18 +0200

    Dodan  class Naloga

---
+++ model.py
@@ -0,0 +1,13 @@
+class Naloga():
+    def __init__(self, tekst, datum=None , ura=None, prioriteta=None):
+        self.tekst = tekst
+        self.datum = datum
+        self.ura = ura
+        self.opravljeno = False
+        self.prioriteta = prioriteta
+        
+
+    
+
+
+

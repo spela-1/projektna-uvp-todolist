631b6bfcf87319a236971679fc93f615ec8bb4ca
spela <a.spela.bernardic@gmail.com>
2020-08-06 19:29:49 +0200

    dodan id projekta

--- model.py
+++ model.py
@@ -34,10 +34,11 @@ class Naloga():
 
 
 class Projekt():
-    def __init__(self, ime):
+    def __init__(self, id, ime):
         self.ime = ime
         self.seznam_nalog = []
         self.naslednji_id = 0
+        self.id = id
     
     def nova_naloga(self, tekst, datum=None , ura=None, prioriteta=None):
         naloga = Naloga(self.naslednji_id, tekst, datum, ura, prioriteta)
@@ -57,19 +58,22 @@ class Projekt():
         slovar = {
             'ime': self.ime,
             'naslednji_id': self.naslednji_id,
-            'seznam_nalog': nov
+            'seznam_nalog': nov,
+            'id': self.id
             }
         return slovar
 
     @classmethod
     def iz_slovarja_pr(cls, pr_slovar):
         ime = pr_slovar['ime']
-        naslednji_id = pr_slovar['nasednji_id']
+        naslednji_id = pr_slovar['naslednji_id']
+        id = pr_slovar['id']
+
         nov = []
         for nal_slovar in pr_slovar['seznam_nalog']:
             naloga = Naloga.iz_slovarja_nal(nal_slovar)
             nov.append(naloga)
-        projekt = cls(ime)
+        projekt = cls(id, ime)
         projekt.seznam_nalog = nov
         projekt.naslednji_id = naslednji_id
         return projekt
@@ -83,6 +87,7 @@ class Uporabnik():
         self.seznam_projektov = []
         self.ime = ime
         self.zasifrirano_geslo = geslo
+        self.naslednji_id_pr = 0
     
     def shrani(self, ime_datoteke):
         nov = []
@@ -91,7 +96,8 @@ class Uporabnik():
         slovar = {
             'ime': self.ime,
             'zasifrirano_geslo': self.zasifrirano_geslo,
-            'seznam_projektov': nov
+            'seznam_projektov': nov,
+            'naslednji_id_pr': self.naslednji_id_pr
         }
         with open(ime_datoteke, 'w',encoding="utf-8") as datoteka:
             json.dump(slovar, datoteka, ensure_ascii=False, indent=4)
@@ -101,8 +107,9 @@ class Uporabnik():
         for projekt in self.seznam_projektov:
             if projekt.ime == ime:
                 return 'error'   
-        projekt = Projekt(ime)
+        projekt = Projekt(self.naslednji_id_pr, ime)
         self.seznam_projektov.append(projekt)
+        self.naslednji_id_pr += 1
 
     def izbrisi_projekt(self, ime):
         for projekt in self.seznam_projektov:
@@ -115,13 +122,15 @@ class Uporabnik():
         with open(ime_datoteke, encoding='utf-8') as datoteka:
             slovar = json.load(datoteka)
         ime = slovar['ime']
-        zasifrirano_geslo = slovar['geslo']
+        naslednji_id_pr = slovar['naslednji_id_pr']
+        zasifrirano_geslo = slovar['zasifrirano_geslo']
         uporabnik = cls(ime, zasifrirano_geslo)
         nov = []
         for pr_slovar in slovar['seznam_projektov']:
             projekt = Projekt.iz_slovarja_pr(pr_slovar)
             nov.append(projekt)
         uporabnik.seznam_projektov = nov
+        uporabnik.naslednji_id_pr = naslednji_id_pr
         return uporabnik
             
 
@@ -135,5 +144,10 @@ class Uporabnik():
 
 u = Uporabnik("Špela", "Sc.1003067")
 u.nov_projekt("Tenis")
-u.seznam_projektov[0].nova_naloga("Timi", [2020,7,21], 16, 5)
+u.seznam_projektov[0].nova_naloga("Timi", [2020,7,21], [16, 0], 5)
 print(u.shrani("test.json"))
+
+'''
+u2 = Uporabnik.nalozi_stanje("test.json")
+print(u2.shrani("test2.json"))
+'''
